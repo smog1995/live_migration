84137333ccc0d84bcb850f9b9fb70aa9c62f582f
smog1995 <ma1653394883@outlook.com>
2024-08-02 17:00:12 +0800

    添加了统计吞吐量的功能

--- system/migration_stat.h
+++ system/migration_stat.h
@@ -26,6 +26,11 @@ public:
 		local_commit_txn = 0;
 		remote_commit_txn = 0;
 		imitate_commit_txn = 0;
+		throughput_stats_size = 0;
+		for (int i = 0; i < throughput_stats_size; i++) {
+			current_throughput.push_back(0);
+			interval_throughput.emplace_back(vector<int>());
+		}
 	// cout << "duration为一秒" << limit_block_overtime.count() << endl;
     }
 
@@ -36,6 +41,8 @@ public:
 	void 		removeTxn(txnid_t txn_id);
 	void 		setUnblockedTxn(txnid_t txn_id);
 	void		abortOvertimeTxn(uint64_t thd_id, vector<txnid_t> &overtime_txns);
+
+	void caculateIntervalThroughput();
 	void		printStats();
     int start_txn; //   remote_txn + local_txn + imitate_txn = start_txn   
 	int local_txn;
@@ -43,12 +50,18 @@ public:
 	int imitate_txn; 
 	// 事务中止统计
 
+	int throughput_stats_size;
     int local_abort_txn;
 	int local_commit_txn;
 	int remote_commit_txn;
     int remote_abort_txn;
     int imitate_abort_txn;
 	int imitate_commit_txn;
+	
+
+	vector<int> current_throughput;
+//  interval为5s的throughput
+	vector<int,vector<int>> interval_throughput;
 private:
     	//  事务阻塞超时处理
 	unordered_map<txnid_t, unique_ptr<TxnEntry>> blocked_txns;
